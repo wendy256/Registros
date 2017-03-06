c51f065b06c562c6db4cebf27d290c9fcead73d8
AlvarezOmar <alvarezomar35@hotmail.com>
2017-03-06 01:13:15 -0600

    Registro
    registro

---
+++ BaseJs/README.md
@@ -0,0 +1,10 @@
+﻿
+
+Equipo: Omar Alvarez Mendoza y Wendy del Carmen Puc Tun Isma-4. 
+
+Esqueleto de una aplicación base para usar Nodejs y AngularJs
+
+Requisitos en el servidor
+
+- Nodejs
+- Servidor de Base de Datos PostgreSQL
---
+++ BaseJs/database/database.sql
@@ -0,0 +1,16 @@
+﻿-- Table: public.registros
+
+-- DROP TABLE public.registros;
+
+CREATE TABLE public.registros
+(
+  id integer NOT NULL DEFAULT nextval('registros_id_seq'::regclass),
+  nombre text,
+  apellido_paterno text,
+  apellido_materno text
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.registros
+  OWNER TO "Omar";
